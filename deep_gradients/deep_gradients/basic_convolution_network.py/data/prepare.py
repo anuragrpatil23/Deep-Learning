b295939cd6d2bdb7438410a35b4a4cea54ceab4c
Anurag Patil <patila06@Anurags-Work-MacBook-Pro.local>
2024-06-11 19:15:42 -0400

    Pit stop 1

---
+++ deep_gradients/deep_gradients/basic_convolution_network.py/data/prepare.py
@@ -0,0 +1,9 @@
+"""
+Please the mnist dataset to be used with Resnet-50. Will save train.bin and val.bin containing the ids and meta.pkl containing the encoder and decoder and some other related info. 
+"""
+
+import os 
+import pickle
+import requests
+import numpy
+
